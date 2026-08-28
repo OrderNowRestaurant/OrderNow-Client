73ba5af819b330f3e0272b199f20cfe916ff13ce
VRLuis <luisvillarroldan1010@gmail.com>
2026-08-28 16:17:32 +0200

    feat: creating a workflow in order to deploy

--- src/services/baseApiService.ts
+++ src/services/baseApiService.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080/api/public';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const baseApiService = async <T>(endpoint: string, options?: RequestInit): Promise<T> => {
     const response = await fetch(`${BASE_URL}${endpoint}`, {
